47eaf8b47b39e91eb1535954cb06c2a9cacf83b6
Nadun Atugoda <nadunatugoda123@gmail.com>
2024-09-11 21:47:54 +0530

    commit the code

--- play.js
+++ play.js
@@ -1,15 +1,3 @@
-// var name = 'Nadun'
-// var age = 24
-// var hashobbies = true;
-
-// function summarizingthedetails(username,theage,hobbiesoftheuser){
-//     return (
-//         'Name is ' +username+ 'age is ' + theage + 'and having the hobbies is : ' +hobbiesoftheuser
-//     );
-// }
-
-// console.log(summarizingthedetails(name,age,hobbies));
- 
 var name ='nadun'
 var age=24
 var hobbieis = true
@@ -18,9 +6,8 @@ function summarizingthedetails(username,age,hobbies){
     return(
         'name is ' +username+ 'age is ' +age+ 'the hobbie is' +hobbies
     );
-
-   
 }
 console.log(summarizingthedetails(name,age,hobbieis))
+
 
 
