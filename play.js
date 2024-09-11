7264261032fae7aa16b72c705bbe5a8e8c9791ff
Nadun Atugoda <nadunatugoda123@gmail.com>
2024-09-11 21:44:50 +0530

    first commit

---
+++ play.js
@@ -0,0 +1,26 @@
+// var name = 'Nadun'
+// var age = 24
+// var hashobbies = true;
+
+// function summarizingthedetails(username,theage,hobbiesoftheuser){
+//     return (
+//         'Name is ' +username+ 'age is ' + theage + 'and having the hobbies is : ' +hobbiesoftheuser
+//     );
+// }
+
+// console.log(summarizingthedetails(name,age,hobbies));
+ 
+var name ='nadun'
+var age=24
+var hobbieis = true
+
+function summarizingthedetails(username,age,hobbies){
+    return(
+        'name is ' +username+ 'age is ' +age+ 'the hobbie is' +hobbies
+    );
+
+   
+}
+console.log(summarizingthedetails(name,age,hobbieis))
+
+
