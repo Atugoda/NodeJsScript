4459796272a2ef04e76cbf28c925b98e49583b14
Nadun Atugoda <nadunatugoda123@gmail.com>
2024-09-11 22:21:08 +0530

    the error is beacuse the hobbieis constant. const cannot be changed at the code.

--- play.js
+++ play.js
@@ -1,13 +1,13 @@
 var name ='nadun'
 var age=24
-var hobbieis = true
+const hobbieis = 'reading books'
+
+name = 'sanduni'
+hobbieis = true
 
 function summarizingthedetails(username,age,hobbies){
     return(
-        'name is ' +username+ 'age is ' +age+ 'the hobbie is' +hobbies
+        'name is ' +username+ ' age is ' +age+ ' the hobbie is ' +hobbies
     );
 }
 console.log(summarizingthedetails(name,age,hobbieis))
-
-
-
