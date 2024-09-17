b64518827dfea2a92d2ebf04885c92f1ee3ca18e
Nadun Atugoda <nadunatugoda123@gmail.com>
2024-09-17 09:48:09 +0530

    if and only if the paranthesis having the return function, then the statement can be written as follows.

---
+++ arrowwithreturn.js
@@ -0,0 +1,9 @@
+const add = (a,b)=> {
+    return (a+b);
+}
+console.log(add(5,6));
+
+//if and only if the paranthesis having the return function, then the statement can be written as follows.
+const mul = (d,c) => (d*c);
+
+console.log(mul(4,5));
---
+++ random.js
@@ -0,0 +1,3 @@
+const addRandom=()=>1+2+3+4;
+
+console.log(addRandom());
