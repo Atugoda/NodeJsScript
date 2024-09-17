87ae79ab640acc87092caacfb4d2357aacbe72fb
Nadun Atugoda <nadunatugoda123@gmail.com>
2024-09-17 19:00:34 +0530

    arrow functionis refered globally, not locally.

---
+++ object.js
@@ -0,0 +1,16 @@
+const person = {
+    name: ' nadun',
+    age: 24,
+    // greet:function(){
+    //     console.log('hi im ' +this.name);
+    // }
+        greet(){
+            console.log('hi im ' +this.name);
+        }
+
+
+};
+
+person.greet();
+// console.log(person);
+//arrow functionis refered globally, not locally.
