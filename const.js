a24ab445a54563ae712222c9199bb7081d100330
Nadun Atugoda <nadunatugoda123@gmail.com>
2024-09-17 08:35:25 +0530

    const.js

---
+++ const.js
@@ -0,0 +1,14 @@
+const name = 'Nadun';
+let age = 24;
+const hoshobbies = true;
+
+age = 30
+
+const summarizeUser = function (Usernameis,Userageis,Userhobbieis){
+    return(
+        'Name is ' + Usernameis + 'age is ' +age+ 'haing the hobbies ' +hoshobbies
+    );
+
+}
+
+console.log(summarizeUser(name,age,hoshobbies));
