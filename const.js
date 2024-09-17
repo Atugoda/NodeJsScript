0dd7671f4d985f6467d863a2f1f5d1a3c49a4d91
Nadun Atugoda <nadunatugoda123@gmail.com>
2024-09-17 09:44:18 +0530

    randomly added numbers

--- const.js
+++ const.js
@@ -11,11 +11,23 @@ const summarizeUser =  (Usernameis,Userageis,Userhobbieis)=>{
 
 }
 //use of arrow functions 
+
 // const add=(a,b)=>{
 //     return a+b;
 // };
 
 const add=(a,b)=>(a+b);
+//if only having the return statement, then this can be wirtten as the arrow function
 
-console.log(add(1,2));
+const addOne = (a) =>  (a+1);
+
+//havin the random without the arguments 
+const addRandom=()=> 1+2+3;
+
+console.log(addOne(9))
+//this is because the arrowfunction has the variable and the addOne function name for the printing value.
+
+// console.log(add(1,2));
 console.log(summarizeUser(name,age.hoshobbies))
+//printing the randomly added numbers 
+console.log(addRandom());
